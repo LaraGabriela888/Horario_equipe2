a8b28ee2f87332c5a0e48612e8d6e85c7c873725
LaraGabriela888 <lara.zembruski@gmail.com>
2024-10-04 09:00:53 -0300

    .

---
+++ php_action/db_connect.php
@@ -0,0 +1,15 @@
+<?php
+//conexão com o banco de dados
+$servername = "localhost";
+$username = "root";
+$password = "1234";
+$db_name = "mercado_gideon";
+
+$connect = mysqli_connect($servername, $username, $password, $db_name);
+mysqli_set_charset($connect, "utf8");
+
+
+if(mysqli_connect_error()){
+    echo "Erro na conexão:  ". mysqli_connect_error();
+}
+?>
