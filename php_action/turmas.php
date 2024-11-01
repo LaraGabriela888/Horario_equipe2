c1d4e5b3e2c112a556ab33ea66dd408c98cb2f1c
LaraGabriela888 <lara.zembruski@gmail.com>
2024-11-01 09:34:04 -0300

    Update turmas.php

--- php_action/turmas.php
+++ php_action/turmas.php
@@ -5,18 +5,12 @@ require_once 'db_connect.php';
 
 // CADASTRAR UM NOVO HORARIO
  if(isset($_POST['btn-registrar'])):
-    $id = mysqli_escape_string($connect, $_POST['id_horario']);
-    $id_turma = mysqli_escape_string($connect, $_POST['id_turma']);
-    $id_materia = mysqli_escape_string($connect, $_POST['id_materia']);
-    $id_professor = mysqli_escape_string($connect, $_POST['id_professor']);
-    $dia = mysqli_escape_string($connect, $_POST['dia']);
-    $turno = mysqli_escape_string($connect, $_POST['turno']);
-    $hora_inicio = mysqli_escape_string($connect, $_POST['hora_inicio']);
-    $hora_fim = mysqli_escape_string($connect, $_POST['hora_fim']);
+    $id = mysqli_escape_string($connect, $_POST['id_turma']);
+    $nome = mysqli_escape_string($connect, $_POST['nome']);
     
 //comando sql para inserir dados
-$sql = "INSERT INTO horarios (id_turma, id_materia, id_professor, dia, turno, hora_inicio, hora_fim) 
-        VALUES ('$id_turma', '$id_materia', '$id_professor', '$dia', '$turno', '$hora_inicio', '$hora_fim')";
+$sql = "INSERT INTO horarios (nome) 
+        VALUES ('$nome')";
 
 $id = "SELECT id_horario FROM horarios WHERE id_horario= '$id'";
 
