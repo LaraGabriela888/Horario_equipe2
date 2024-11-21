bd6caebbecba89bff7f3060262263a892f36189f
LaraGabriela888 <lara.zembruski@gmail.com>
2024-11-21 14:46:36 -0300

    Create materiaCa.php

---
+++ materiaCa.php
@@ -0,0 +1,19 @@
+<?php include_once 'includes/header.php'?>
+
+<div class="content-add">    
+<!-- CADASTRAR UMA NOVA MATERIA -->
+<div class="content">
+<h1>Cadastrar Materia</h1>
+<form action="php_action/materias.php" method="POST" class="content-form">
+  
+<div class="input-item">
+<label for="nome"><i class="bi bi-person"></i>Nome da Materia</label>
+<input type="text" name="nome">
+</div>
+
+<button type="submit" name="btn-registrar" class="btn-registrar">Cadastrar</button>
+</form>
+</div>
+</div>
+
+<?php include_once 'includes/footer.php'?>
