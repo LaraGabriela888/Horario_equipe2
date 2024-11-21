1d0eea5d7027aca999c91165d96eb8f79a4c1c13
LaraGabriela888 <lara.zembruski@gmail.com>
2024-11-21 15:12:59 -0300

    Create deleteH.php

---
+++ php_action/deleteH.php
@@ -0,0 +1,21 @@
+<?php
+// conexão
+session_start();// sessão SEMPRE em primeiro
+require_once 'db_connect.php';
+
+// DELETAR UM HORARIO
+if(isset($_POST['btn-deletar'])):
+    $id = mysqli_escape_string($connect, $_POST['id_horario']);
+//comando sql para deletar dados
+$sql = "DELETE FROM horarios WHERE id_horario = '$id'";
+
+if(mysqli_query($connect, $sql))://
+    $_SESSION['mensagem'] = "Deletado com sucesso!";
+    header('Location: ../produtos.php?sucesso');//levar par index com sucesso
+else:
+    $_SESSION['mensagem'] = "Erro ao deletar";  
+    header('Location: ../produtos.php?erro');//caso a conexão não funcione erro
+endif;
+endif;
+
+?>
