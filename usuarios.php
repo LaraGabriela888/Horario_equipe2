0ffa7e15863ef14bf3e7f507f5dc6d3e4105438c
LaraGabriela888 <lara.zembruski@gmail.com>
2024-11-28 11:41:21 -0300

    .ghhgf

--- usuarios.php
+++ usuarios.php
@@ -1,7 +1,7 @@
 <?php 
 include_once 'includes/mensagem.php';
 require_once 'php_action/db_connect.php';
-require_once 'includes/header.php';
+include 'includes/header.php';
 ?>
 
     <!-- MOSTRAR OS USUARIOS -->
