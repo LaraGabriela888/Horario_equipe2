94042dfdc16999155f5aaef050037cb1e31fd103
LaraGabriela888 <lara.zembruski@gmail.com>
2024-11-29 08:48:31 -0300

    .

--- includes/header.php
+++ includes/header.php
@@ -3,10 +3,6 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="CSS/apr.css">
 
     <title>Document</title>
